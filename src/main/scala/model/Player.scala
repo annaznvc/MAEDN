0958b78cdb4c16440efa67c47e73c3f7dd0ab913
Anna Zynovchenko <annazynovchenko05@gmail.com>
2025-04-07 16:37:34 +0200

    Update zu Player und Playertest

--- src/main/scala/model/Player.scala
+++ src/main/scala/model/Player.scala
@@ -1,3 +1,15 @@
 package model
 
-case class Player(id: Int, name: String, color: Color, figures: List[Figure])
+case class Player(
+  id: Int,
+  name: String,
+  color: Color,
+  figures: List[Figure],
+  status: PlayerStatus = Active
+):
+  require(name.nonEmpty, "Name must not be empty")
+  require(figures.size == 4, "Player must have exactly 4 figures")
+  require(figures.forall(_.color == color), "All figures must match the player's color")
+
+  def figureById(figureId: Int): Option[Figure] =
+    figures.find(_.id == figureId)
--- src/main/scala/model/PositionState.scala
+++ src/main/scala/model/PositionState.scala
@@ -1,6 +1,11 @@
 package model
 
 sealed trait PositionState
+
 case object Home extends PositionState
+
+case class Start(pos: Position) extends PositionState
+
 case class OnBoard(pos: Position) extends PositionState
+
 case object Finished extends PositionState
