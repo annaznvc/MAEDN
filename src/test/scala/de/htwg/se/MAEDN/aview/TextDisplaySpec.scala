cd2cab61a8cc2d59affb6160c84f2bdfa41ea524
Layth <la911ala@htwg-konstanz.de>
2025-05-12 11:53:15 +0200

    Refactor TextDisplaySpec for improved readability and consistency

--- src/test/scala/de/htwg/se/MAEDN/aview/TextDisplaySpec.scala
+++ src/test/scala/de/htwg/se/MAEDN/aview/TextDisplaySpec.scala
@@ -1,28 +1,35 @@
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 import de.htwg.se.MAEDN.aview.TextDisplay
-import de.htwg.se.MAEDN.model.{Board, Field, FieldType, Player, Figure, IState, State}
+import de.htwg.se.MAEDN.model.{
+  Board,
+  Field,
+  FieldType,
+  Player,
+  Figure,
+  IState,
+  State
+}
 import de.htwg.se.MAEDN.util.Color
 import de.htwg.se.MAEDN.model.Manager
 import de.htwg.se.MAEDN.model.BoardFactory
-
-
 
 class TextDisplaySpec extends AnyWordSpec with Matchers {
 
   "TextDisplay" should {
 
     "clear the terminal with ANSI code" in {
-      TextDisplay.clearTerminal() should include ("\u001b[2J")
+      TextDisplay.clearTerminal() should include("\u001b[2J")
     }
 
-   "print cover for Config state" in {
-    val dummy = new de.htwg.se.MAEDN.model.Manager {
+    "print cover for Config state" in {
+      val dummy = new de.htwg.se.MAEDN.model.Manager {
         override val controller = null
         override val moves = 7
         override val rolled = 0
         override val state = de.htwg.se.MAEDN.model.State.Config
-        override val board = de.htwg.se.MAEDN.model.Board(Vector.empty, Vector.empty)
+        override val board =
+          de.htwg.se.MAEDN.model.Board(Vector.empty, Vector.empty)
         override val players = List.empty
 
         override def getPlayerCount = 3
@@ -41,21 +48,22 @@ class TextDisplaySpec extends AnyWordSpec with Matchers {
         override def playDice() = this
         override def playNext() = this
         override def moveFigure() = this
-    }
+      }
 
-    val output = de.htwg.se.MAEDN.aview.TextDisplay.printCover(dummy)
-    output should include ("Config")
-    output should include ("3 players")
-    output should include ("9x9 board")
+      val output = de.htwg.se.MAEDN.aview.TextDisplay.printCover(dummy)
+      output should include("Config")
+      output should include("3 players")
+      output should include("9x9 board")
     }
 
     "print cover for Menu state" in {
-    val dummy = new de.htwg.se.MAEDN.model.Manager {
+      val dummy = new de.htwg.se.MAEDN.model.Manager {
         override val controller = null
         override val moves = 5
         override val rolled = 0
         override val state = de.htwg.se.MAEDN.model.State.Menu
-        override val board = de.htwg.se.MAEDN.model.Board(Vector.empty, Vector.empty)
+        override val board =
+          de.htwg.se.MAEDN.model.Board(Vector.empty, Vector.empty)
         override val players = List.empty
 
         override def getPlayerCount = 2
@@ -74,26 +82,22 @@ class TextDisplaySpec extends AnyWordSpec with Matchers {
         override def playDice() = this
         override def playNext() = this
         override def moveFigure() = this
+      }
+
+      val output = de.htwg.se.MAEDN.aview.TextDisplay.printCover(dummy)
+      output should include("Menu")
+      output should include("2 players")
+      output should include("8x8 board")
     }
-
-    val output = de.htwg.se.MAEDN.aview.TextDisplay.printCover(dummy)
-    output should include ("Menu")
-    output should include ("2 players")
-    output should include ("8x8 board")
-    }
-
-
-
-
-
 
     "print cover for Running state" in {
-    val dummy = new de.htwg.se.MAEDN.model.Manager {
+      val dummy = new de.htwg.se.MAEDN.model.Manager {
         override val controller = null
         override val moves = 10
         override val rolled = 0
         override val state = de.htwg.se.MAEDN.model.State.Running
-        override val board = de.htwg.se.MAEDN.model.Board(Vector.empty, Vector.empty)
+        override val board =
+          de.htwg.se.MAEDN.model.Board(Vector.empty, Vector.empty)
         override val players = List.empty
 
         override def getPlayerCount = 4
@@ -112,20 +116,20 @@ class TextDisplaySpec extends AnyWordSpec with Matchers {
         override def playDice() = this
         override def playNext() = this
         override def moveFigure() = this
-    }
+      }
 
-    val output = de.htwg.se.MAEDN.aview.TextDisplay.printCover(dummy)
-    output should include ("Running")
-    output should include ("4 players")
-    output should include ("11x11 board")
+      val output = de.htwg.se.MAEDN.aview.TextDisplay.printCover(dummy)
+      output should include("Running")
+      output should include("4 players")
+      output should include("11x11 board")
     }
 
     //
 
     "print config layout" in {
       val output = TextDisplay.printConfig(2, 2, 8)
-      output should include ("Press [Space] to start a new game")
-      output should include ("2")
+      output should include("Press [Space] to start a new game")
+      output should include("2")
     }
 
     "print board with fields and homes" in {
@@ -135,80 +139,68 @@ class TextDisplaySpec extends AnyWordSpec with Matchers {
       val board = Board(Vector.fill(4)(field), Vector.fill(4)(home))
 
       val output = TextDisplay.printBoard(board)
-      output should include ("Home Fields")
-      output should include ("Main Board")
-      output should include ("Red:")
-      output should include ("F1") // die Figur-ID
+      output should include("Home Fields")
+      output should include("Main Board")
+      output should include("Red:")
+      output should include("F1") // die Figur-ID
     }
 
     "render all field colors via colorCode in printBoard" in {
-        val red = Player(1, List(Figure(1, null)), Color.RED)
-        val blue = Player(2, List(Figure(1, null)), Color.BLUE)
-        val green = Player(3, List(Figure(1, null)), Color.GREEN)
-        val yellow = Player(4, List(Figure(1, null)), Color.YELLOW)
+      val red = Player(1, List(Figure(1, null)), Color.RED)
+      val blue = Player(2, List(Figure(1, null)), Color.BLUE)
+      val green = Player(3, List(Figure(1, null)), Color.GREEN)
+      val yellow = Player(4, List(Figure(1, null)), Color.YELLOW)
 
-        val redField = Field(None, FieldType.Normal, Color.RED)
-        val blueField = Field(None, FieldType.Normal, Color.BLUE)
-        val greenField = Field(None, FieldType.Normal, Color.GREEN)
-        val yellowField = Field(None, FieldType.Normal, Color.YELLOW)
+      val redField = Field(None, FieldType.Normal, Color.RED)
+      val blueField = Field(None, FieldType.Normal, Color.BLUE)
+      val greenField = Field(None, FieldType.Normal, Color.GREEN)
+      val yellowField = Field(None, FieldType.Normal, Color.YELLOW)
 
-        val board = Board(
-            fields = Vector(redField, blueField, greenField, yellowField),
-            homeFields = Vector.empty
-        )
+      val board = Board(
+        fields = Vector(redField, blueField, greenField, yellowField),
+        homeFields = Vector.empty
+      )
 
-        val output = TextDisplay.printBoard(board)
+      val output = TextDisplay.printBoard(board)
 
-        output should include ("Red")
-        output should include ("Blue")
-        output should include ("Green")
-        output should include ("Yellow")
-        }
+      output should include("Red")
+      output should include("Blue")
+      output should include("Green")
+      output should include("Yellow")
+    }
 
     "render all FieldTypes in printBoard" in {
-    val homeFields = Vector(
+      val homeFields = Vector(
         Field(None, FieldType.Home, Color.RED),
         Field(None, FieldType.Home, Color.BLUE),
         Field(None, FieldType.Home, Color.GREEN),
         Field(None, FieldType.Home, Color.YELLOW)
-    )
+      )
 
-    val startField = Field(None, FieldType.Start, Color.RED)
-    val goalField = Field(None, FieldType.Goal, Color.RED)
+      val startField = Field(None, FieldType.Start, Color.RED)
+      val goalField = Field(None, FieldType.Goal, Color.RED)
 
-    val board = Board(
+      val board = Board(
         fields = Vector(startField, goalField),
         homeFields = homeFields
-    )
+      )
 
-    val output = TextDisplay.printBoard(board)
+      val output = TextDisplay.printBoard(board)
 
-    output should include ("H ")
-    output should include ("S ")
-    output should include ("G ")
+      output should include("H ")
+      output should include("S ")
+      output should include("G ")
     }
 
     "render fallback player labels beyond labels list" in {
-        val homeFields = Vector.fill(5)(Field(None, FieldType.Home, Color.RED))
-        val board = Board(Vector.empty, homeFields)
+      val homeFields = Vector.fill(5)(Field(None, FieldType.Home, Color.RED))
+      val board = Board(Vector.empty, homeFields)
 
-        val output = TextDisplay.printBoard(board)
+      val output = TextDisplay.printBoard(board)
 
-        output should include ("Player5")
-        }
-
-
-}
-
-
-
-
-
-
-
-   
-
-
+      output should include("Player5")
+    }
 
   }
 
+}
